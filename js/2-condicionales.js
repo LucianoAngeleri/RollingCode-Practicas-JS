43133ba82d77ad4b543ce21cee912dd12f36ef19
Luciano Angeleri <lcnang@gmail.com>
2023-02-28 18:19:01 -0300

    Se añadió la rtasnformación del input del usuario a número entero

--- js/2-condicionales.js
+++ js/2-condicionales.js
@@ -8,7 +8,11 @@ if (condicion){
 }
 */
 //Solicitar la edad del ususario e indicar por pantalla si puede votar o no
-let edad = prompt("Ingrese su edad: ");
+let edad = parseInt(prompt("Ingrese su edad: "));
+//parseint: Transformar una cadena de texto en numero
+console.log(edad);
+console.log(typeof(edad));
+
 if (edad>=16){
     document.write(`El usuario tiene ${edad} años, entonces PUEDE votar` );
 }else{
