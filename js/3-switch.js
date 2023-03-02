c819c272b778b80a96196c329301766d0ad14981
Luciano Angeleri <lcnang@gmail.com>
2023-03-02 17:21:28 -0300

    Añadido y linkeado el script de la práctica de switch

---
+++ js/3-switch.js
@@ -0,0 +1,19 @@
+/*
+let opcion = ""
+switch(opcion){
+    case 1:
+        bloque de código para ejercutar en el caso 1
+        break;
+    case 2:
+        bloque de código para ejercutar en el caso 2
+        break;
+    case 3:
+        bloque de código para ejercutar en el caso 3
+        break;
+    case "deposito":
+        bloque de código para ejercutar en el caso 1
+        break;
+    case default:
+        bloque de código para ejercutar por defecto (si no se cumple ningun caso)
+}
+*/
