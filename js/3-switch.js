b5fbd8552865c83321000a87b8db9e86f142a6dd
Luciano Angeleri <lcnang@gmail.com>
2023-03-02 17:47:02 -0300

    Se agregó las opciones de los diferentes menus

--- js/3-switch.js
+++ js/3-switch.js
@@ -17,3 +17,48 @@ switch(opcion){
         bloque de código para ejercutar por defecto (si no se cumple ningun caso)
 }
 */
+
+/*
+Solicitar al usuario que seleccione un día de la semana,
+y le vamos a sugerir un menú para cada día:
+Plato principal
+Bebida
+Postre
+*/
+const dia = prompt("Seleccione un día de la semana: 1-Lunes, 2-Miercoles, 3-Viernes: ");
+
+switch (dia){
+  case "1":
+    document.write(`
+        <h1>Lunes</h1>
+        <h2>Menú</h2>
+        <ul>
+            <li>Plato Principal:Milanesa con pure</li>
+            <li>Bebida:Coca-Cola</li>
+            <li>Postre:Flan con dulce de leche</li>
+        </ul>`);
+    break;
+  case "2":
+    document.write(`
+        <h1>Miercoles</h1>
+        <h2>Menú</h2>
+        <ul>
+            <li>Plato Principal:Filet de merluza con papas</li>
+            <li>Bebida:Sprite</li>
+            <li>Postre:Brownie con Helado</li>
+        </ul>`);
+    break;
+  case "3":
+    document.write(`
+        <h1>Viernes</h1>
+        <h2>Menú</h2>
+        <ul>
+            <li>Plato Principal:Pizza</li>
+            <li>Bebida:Cerveza</li>
+            <li>Postre:Helado</li>
+        </ul>`);
+    break;
+  default:
+    document.write(`<h1>No eligio un dia valido</h1>`);
+    break;
+}
